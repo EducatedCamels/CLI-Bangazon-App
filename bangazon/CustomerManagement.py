e2905899a7d483c2569548e69f76bfd338917493
Dara Thomas <darathomas@Daras-MacBook-Air.local>
2017-05-08 11:23:45 -0500

    got started

--- bangazon/CustomerManagement.py
+++ bangazon/CustomerManagement.py
@@ -1,1 +1,23 @@
-placeholder
+import sys
+
+class Customer():
+
+	def save_customer_to_database():
+
+
+	def get_customer():
+
+
+	def get_all_customers():
+		
+
+	def select_active_customer():
+
+
+
+
+ #active_customer = null
+
+
+
+if __name__ == '__main__':
