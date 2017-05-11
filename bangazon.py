7f959fff8e66e5f80d63b34ef04a542e4357675d
Bri Wyatt <mbriwyatt@gmail.com>
2017-05-11 11:36:45 -0500

    fied bangazon.py file

--- bangazon.py
+++ bangazon.py
@@ -31,8 +31,15 @@ class Bangazon():
 		choice = input(">> ")
 
 
-		# if choice == "1":
 
+        if choice == "1":
+            first_name = input("First Name: ")
+            last_name = input("Last Name: ")
+            address = input("Address: ")
+            phone_number = input("Phone Number: ")
+
+            save_customer_to_database(first_name, last_name, address, phone_number)
+        
 
 
 
