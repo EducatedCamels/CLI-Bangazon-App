3d2798d59d09290ad1ceb5ec02a51b9fc8df75d9
Dara Thomas <darathomas@Daras-MacBook-Air.local>
2017-05-11 12:12:19 -0500

    fixed indentation error so Option 3 doesn't ask you to enter a customer name etc

--- bangazon.py
+++ bangazon.py
@@ -31,11 +31,11 @@ class Bangazon():
 
         if choice == "1":
             first_name = input("First Name: ")
-        last_name = input("Last Name: ")
-        address = input("Address: ")
-        phone_number = input("Phone Number: ")
+            last_name = input("Last Name: ")
+            address = input("Address: ")
+            phone_number = input("Phone Number: ")
 
-        save_customer_to_database(first_name, last_name, address, phone_number)
+            save_customer_to_database(first_name, last_name, address, phone_number)
         
 
 
